13387f901beb3c5e6018937c612ec44b5c33d420
baimamboukar <baimamboukar@gmail.com>
2022-01-29 23:19:48 +0100

    added navigation rail

---
+++ lib/screens/home.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: <Widget>[
+          NavigationRail(
+            selectedIndex: 0,
+            onDestinationSelected: (int index) {},
+            labelType: NavigationRailLabelType.selected,
+            destinations: const [
+              NavigationRailDestination(
+                icon: Icon(Icons.favorite_border),
+                selectedIcon: Icon(Icons.favorite),
+                label: Text('First'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.bookmark_border),
+                selectedIcon: Icon(Icons.book),
+                label: Text('Second'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.star_border),
+                selectedIcon: Icon(Icons.star),
+                label: Text('Third'),
+              ),
+            ],
+          ),
+          const VerticalDivider(thickness: 1, width: 1),
+          // This is the main content.
+          const Expanded(
+            child: Center(
+              child: Text("Defterre Gimmi"),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
