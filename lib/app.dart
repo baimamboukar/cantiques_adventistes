03d8b4c723a28e9062525f7361cfd943a6ad5ee7
baimamboukar <baimamboukar@gmail.com>
2022-01-29 23:08:37 +0100

    created app file

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CantiquesAdventistes extends StatelessWidget {
+  const CantiquesAdventistes({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Cantiques Adventistes', home: Home());
+  }
+}
