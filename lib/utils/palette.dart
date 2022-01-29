bf90949a070d1e6da361d1c664b73b122b01e52a
baimamboukar <baimamboukar@gmail.com>
2022-01-29 23:27:32 +0100

    palette colors added

---
+++ lib/utils/palette.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static Color primary = const Color(0xFF38C7FF);
+  static Color secondary = const Color(0xDA08DDD2);
+  static Color light = const Color(0xFFFFFFFF);
+  static Color dark = const Color(0x000A0A0A);
+}
