45fa96b2780f58c8bafd91b5bfb587a90e6c9a6a
baimamboukar <baimamboukar@gmail.com>
2022-01-30 00:26:53 +0100

    defterre gimmi created

---
+++ lib/screens/defterre_gimmi.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DefterreGimmi extends StatelessWidget {
+  const DefterreGimmi({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Column(
+        children: const [],
+      ),
+    ));
+  }
+}
