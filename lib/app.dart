b861e5316844621dea43817d78474762ab42791b
baimamboukar <baimamboukar@gmail.com>
2022-01-29 23:13:13 +0100

    font added

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CantiquesAdventistes extends StatelessWidget {
   const CantiquesAdventistes({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Cantiques Adventistes', home: Home());
+    return MaterialApp(
+      title: 'Cantiques Adventistes',
+      useInheritedMediaQuery: true,
+      debugShowCheckedModeBanner: false,
+      restorationScopeId: 'cantiques_adventistes',
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: GoogleFonts.xanhMonoTextTheme(),
+      ),
+      home: Home(),
+    );
   }
 }
