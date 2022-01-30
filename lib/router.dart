6b832deccbefe9d19a55301c173445d245d31414
baimamboukar <baimamboukar@gmail.com>
2022-01-30 02:08:41 +0100

    adding cantique view route

--- lib/router.dart
+++ lib/router.dart
@@ -6,9 +6,8 @@ import 'package:cantiques_adventistes/screens/screens.dart';
   replaceInRouteName: 'cantiques adventistes',
   routes: <AutoRoute>[
     AutoRoute(page: Home, initial: true, path: '/'),
-    AutoRoute(page: DefterreGimmi, path: "/defterre-gimmi", children: [
-      AutoRoute(page: CantiqueView, path: "defterre-gimmi/:number"),
-    ]),
+    AutoRoute(page: CantiqueView, path: "/cantique-view/:number"),
+    AutoRoute(page: DefterreGimmi, path: "/defterre-gimmi", children: []),
   ],
 )
 class $AppRouter {}
