6c587d45636faed9f14583d945f57cdb90cb2632
baimamboukar <baimamboukar@gmail.com>
2022-01-30 01:54:09 +0100

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:cantiques_adventistes/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const CantiquesAdventistes());
+  runApp(const ProviderScope(child: CantiquesAdventistes()));
 }
