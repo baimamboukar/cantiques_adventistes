a3988b77d516ea745c9294924630f28fb65d412d
baimamboukar <baimamboukar@gmail.com>
2022-01-30 01:53:52 +0100

    navigations reconfig

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,18 @@
-import 'package:cantiques_adventistes/router.dart';
-import 'package:cantiques_adventistes/screens/screens.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import 'generated_router.dart';
 
 class CantiquesAdventistes extends StatelessWidget {
   const CantiquesAdventistes({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final _appRouter = $AppRouter();
-    return MaterialApp(
+    final _appRouter = AppRouter();
+    return MaterialApp.router(
       title: 'Cantiques Adventistes',
+      routerDelegate: _appRouter.delegate(),
+      routeInformationParser: _appRouter.defaultRouteParser(),
       useInheritedMediaQuery: true,
       debugShowCheckedModeBanner: false,
       restorationScopeId: 'cantiques_adventistes',
