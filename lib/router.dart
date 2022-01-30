ce742c8d1172475a6b02887716468fb8ecfb9b6d
baimamboukar <baimamboukar@gmail.com>
2022-01-30 01:54:18 +0100

    adding new routers

--- lib/router.dart
+++ lib/router.dart
@@ -2,11 +2,13 @@ import 'package:auto_route/auto_route.dart';
 import 'package:cantiques_adventistes/screens/screens.dart';
 
 @MaterialAutoRouter(
+  preferRelativeImports: true,
   replaceInRouteName: 'cantiques adventistes',
   routes: <AutoRoute>[
-    AutoRoute(page: Home, initial: true),
-    AutoRoute(page: DefterreGimmi, path: '/defterre-gimmi'),
-    AutoRoute(page: Home),
+    AutoRoute(page: Home, initial: true, path: '/'),
+    AutoRoute(page: DefterreGimmi, path: "/defterre-gimmi", children: [
+      AutoRoute(page: CantiqueView, path: "defterre-gimmi/:number"),
+    ]),
   ],
 )
 class $AppRouter {}
