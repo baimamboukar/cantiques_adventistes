ae6ee70bef8b1e844cc5d9449d724d72e15982c4
baimamboukar <baimamboukar@gmail.com>
2022-01-30 01:54:47 +0100

    cantique view screen

---
+++ lib/screens/cantique_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:auto_route/auto_route.dart';
+
+class CantiqueView extends StatelessWidget {
+  final int number;
+  const CantiqueView({Key? key, @PathParam('number') required this.number})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: const [],
+        ),
+      ),
+    );
+  }
+}
