b51c9ff2ff2a01ff44209b2247954f74c759c38b
baimamboukar <baimamboukar@gmail.com>
2022-01-30 11:48:05 +0100

    styles added

---
+++ lib/utils/styles.dart
@@ -0,0 +1,10 @@
+import 'package:cantiques_adventistes/utils/palette.dart';
+import 'package:flutter/material.dart';
+
+class Styles {
+  static TextStyle refrain = TextStyle(
+    fontSize: 18,
+    color: Palette.primary,
+    fontStyle: FontStyle.italic,
+  );
+}
