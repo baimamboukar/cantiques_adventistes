e5d3ba80c0fb885910237af9b1921559cc8b2735
baimamboukar <baimamboukar@gmail.com>
2022-01-30 01:54:37 +0100

    router provider

---
+++ lib/providers/router_provider.dart
@@ -0,0 +1,4 @@
+import 'package:cantiques_adventistes/generated_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final routerProvider = Provider<AppRouter>((ref) => AppRouter());
